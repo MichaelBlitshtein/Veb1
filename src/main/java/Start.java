40119b95f79d07ed33d63a59c5d4a6d9da9cc3fd
MichaelBlitshtein <michael.blitshtein@gmail.com>
2022-12-24 11:45:46 +0300

    css+

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -29,4 +29,29 @@ public class Start {
     WebElement el7= wd.findElement(By.className("css-164r41r"));
     WebElement el8 = wd.findElement(By.linkText("Log in"));
     WebElement el9 = wd.findElement(By.className("m2N684FcksCyfT"));
+
+
+    //css
+    WebElement el1c = wd.findElement(By.cssSelector("#user"));       //#  ==  id
+    WebElement el2c = wd.findElement(By.cssSelector(".m2N684FcksCyfT"));   //  . == Class
+    WebElement el3c = wd.findElement(By.cssSelector("[data-testid='header-member-menu-button']"));  // user icon
+
+
+
+    //log in
+    WebElement log1 = wd.findElement(By.cssSelector("[href='/login']"));
+    WebElement log2 = wd.findElement(By.cssSelector("#user"));
+    WebElement log3 = wd.findElement(By.cssSelector("#login"));
+    WebElement log4 = wd.findElement(By.cssSelector("#password"));
+    WebElement log5 = wd.findElement(By.cssSelector("#login-submit"));
+
+
+
+    //
+    WebElement log11 = wd.findElement(By.cssSelector("[aria-label='Trello']"));
+    WebElement log121 = wd.findElement(By.cssSelector("[aria-label $='llo']"));//ends
+    WebElement log31 = wd.findElement(By.cssSelector("[aria-label ^='Tr']"));//starts
+    WebElement log41 = wd.findElement(By.cssSelector("[aria-label *='rell']"));//contains
+WebElement log1 = wd.findElement(By.cssSelector("[href='/login']"));
+
 }
